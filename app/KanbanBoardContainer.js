8d34d945b04af7aaf3f55f5955954259be1ee8e5
RayShaw <784452262@qq.com>
2017-11-24 16:53:28 +0800

    update

--- app/KanbanBoardContainer.js
+++ app/KanbanBoardContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import KanbanBoard from './KanbanBoard';
+import update from 'immutability-helper';
 
 class KanbanBoardContainer extends Component {
   constructor() {
@@ -14,6 +15,8 @@ class KanbanBoardContainer extends Component {
       .then((response) => response.json())
       .then((responseData) => {
         this.setState({ cards: responseData });
+
+        window.state = this.state;
       })
       .catch((error) => {
         console.log('Error fetching and parsing data', error);
@@ -21,15 +24,126 @@ class KanbanBoardContainer extends Component {
   }
 
   addTask(cardId, taskName) {
-    console.log('add');
+    // console.log('add');
+
+    let prevState = this.state;
+
+    let cardIndex = this.state.cards.findIndex(card => card.id === cardId);
+
+    let newTask = { id: Date.now(), name: taskName, done: false }
+
+    let nextState = update(this.state.cards, {
+      [cardIndex]: {
+        tasks: {
+          $push: [newTask]
+        }
+      }
+    });
+
+    this.setState({ cards: nextState });
+
+
+
+    // Call the API to toggle the task on the server
+
+    // fetch(`API_URL.cards.${cardId}/tasks`, {
+    //   method: 'post',
+    //   headers: { 'Content-Type': 'application/json', Authorization: 'any-string-you-like' },
+    //   body: JSON.stringify(newTask)
+    // })
+    //   .then((response) => {
+    //     if (response.ok)
+    //       return response.json();
+    //     else
+    //       throw new Error(`Server response wasn't OK`);
+
+    //   })
+    //   .then(responseData => {
+    //     newTask.id = responseData.id
+    //     this.setState({ cards: nextState });
+    //   })
+    //   .catch(error => {
+    //     console.error('Fetch eror:', error);
+    //     this.setState(prevState);
+    //   })
+
   }
 
   deleteTask(cardId, taskId, taskIndex) {
-    console.log('delete');
+    // console.log('delete');
+
+    let prevState = this.state;
+
+    let cardIndex = this.state.cards.findIndex(
+      card => card.id === cardId
+    );
+
+    // console.log(this.state.cards[cardIndex].tasks[taskIndex]);
+    let nextState = update(this.state.cards, {
+      [cardIndex]: {
+        tasks: { $splice: [[taskIndex, 1]] }
+      }
+    });
+
+    // console.log(nextState);
+    this.setState({ cards: nextState });
+
+
+    // Call the API to remove the rask on the server
+    // fetch(`API_URL.cards.${cardId}/tasks/${taskId}`, {
+    //   method: 'delete',
+    //   headers: { 'Content-Type': 'application/json', Authorization: 'any-string-you-like' }
+    // })
+    //   .then(response => {
+    //     if (!response.ok)
+    //       throw new Error(`Server response wasn't OK`);
+    //   })
+    //   .catch(error => {
+    //     console.error('Fetch eror:', error);
+    //     this.setState(prevState);
+    //   });
+
   }
 
   toggleTask(cardId, taskId, taskIndex) {
-    console.log('toggle');
+    // console.log('toggle');
+
+    let prevState = this.state;
+
+    let cardIndex = this.state.cards.findIndex(card => card.id === cardId);
+    let newDoneValue;
+    let nextState = update(this.state.cards, {
+      [cardIndex]: {
+        tasks: {
+          [taskIndex]: {
+            done: {
+              $apply: (done) => {
+                newDoneValue = !done;
+                return newDoneValue;
+              }
+            }
+          }
+        }
+      }
+    });
+
+    this.setState({ cards: nextState });
+
+    // Call the API to toggle the task on the server
+    // fetch(`API_URL.cards.${cardId}/tasks/${taskId}`, {
+    //   method: 'put',
+    //   headers: { 'Content-Type': 'application/json', Authorization: 'any-string-you-like' },
+    //   body: JSON.stringify({ done: newDoneValue })
+    // })
+    //   .then(response => {
+    //     if (!response.ok) {
+    //       throw new Error(`Server response wasn't OK`);
+    //     }
+    //   })
+    //   .catch(error => {
+    //     console.error('Fetch eror:', error);
+    //     this.setState(prevState);
+    //   });
   }
 
   render() {
